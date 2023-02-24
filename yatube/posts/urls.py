0ad942a85f68f6c7b38534fd7d485bf70ec20531
Михаил Полшков <m@polshkov.ru>
2023-02-24 10:30:53 +0500

    Шаблоны rev.1

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('index', views.index),
+    path('group_list', views.group_list),
     path('group/<slug:slug>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
