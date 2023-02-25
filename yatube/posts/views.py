f9867e51d01314ea0c15c8a2b169185631b8e4f5
Mikhail Polshkov <m@polshkov.ru>
2023-02-25 13:57:57 +0500

    Базы данных и django ORM

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': 'Yatube',
-        'text': 'Это главная страница проекта Yatube'
+        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
     return render(request, template, context)
 
 
-def group_list(request):
-    template = 'posts/group_list.html'
-    return render(request, template)
-
-
 def group_posts(request, slug):
-
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Groups',
+        'title': 'Группы Yatube',
+        'group': group,
+        'posts': posts,
         'text': 'Здесь будет информация о группах проекта Yatube'
     }
-    return HttpResponse(f'Посты сгруппированные по {slug}')
+    return render(request, template, context)
+
