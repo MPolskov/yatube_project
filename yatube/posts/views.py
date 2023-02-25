7508703c5a0704c91b94219d48ffb7afb9144857
Mikhail Polshkov <m@polshkov.ru>
2023-02-25 15:42:37 +0500

    Подготовка к сдаче на ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+
 
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Yatube',
-        'text': 'Это главная страница проекта Yatube',
+        'title': 'Последние обновления на сайте',
         'posts': posts,
     }
     return render(request, template, context)
@@ -18,10 +17,8 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Группы Yatube',
+        'title': f'Записи сообщества {group}',
         'group': group,
         'posts': posts,
-        'text': 'Здесь будет информация о группах проекта Yatube'
     }
     return render(request, template, context)
-
